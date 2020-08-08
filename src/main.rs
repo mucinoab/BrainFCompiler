ddbe288de1557105ce97d17075d3f6098f7d4087
Bruno Muciño <mucinoab@gmail.com>
2020-08-07 19:17:02 -0500

    Bigger example and better errors.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let source = fs::read_to_string(&args[1]).expect("Missing source file.");
     let mut iter = source.chars().peekable();
 
-    let mut output = String::with_capacity(8192);
+    let mut output_asm = String::with_capacity(8192);
     let mut cola = VecDeque::with_capacity(1024);
     let mut label_count = 0;
     let mut consecutive = 1;
 
-    output.push_str(PROLOG);
+    output_asm.push_str(PROLOG);
 
     while let Some(chars) = iter.next() {
         match chars {
@@ -21,7 +21,7 @@ fn main() {
                     iter.next();
                 }
 
-                output.push_str(&format!(" sub r12, {}\n", consecutive));
+                output_asm.push_str(&format!(" sub r12, {}\n", consecutive));
             }
 
             '>' => {
@@ -30,7 +30,7 @@ fn main() {
                     iter.next();
                 }
 
-                output.push_str(&format!(" add r12, {}\n", consecutive));
+                output_asm.push_str(&format!(" add r12, {}\n", consecutive));
             }
 
             '+' => {
@@ -39,7 +39,7 @@ fn main() {
                     iter.next();
                 }
 
-                output.push_str(&format!(" add byte [r12], {}\n", consecutive));
+                output_asm.push_str(&format!(" add byte [r12], {}\n", consecutive));
             }
 
             '-' => {
@@ -48,15 +48,15 @@ fn main() {
                     iter.next();
                 }
 
-                output.push_str(&format!(" sub byte [r12], {}\n", consecutive));
+                output_asm.push_str(&format!(" sub byte [r12], {}\n", consecutive));
             }
 
-            ',' => output.push_str(" call getchar;\n mov [r12], al\n"),
+            ',' => output_asm.push_str(" call getchar;\n mov [r12], al\n"),
 
-            '.' => output.push_str(" mov dil, [r12];\n call putchar\n"),
+            '.' => output_asm.push_str(" mov dil, [r12];\n call putchar\n"),
 
             '[' => {
-                output.push_str(&format!(
+                output_asm.push_str(&format!(
                     "label{0}start:\n cmp byte [r12], 0\n jz label{0}end\n",
                     label_count
                 ));
@@ -65,7 +65,7 @@ fn main() {
                 label_count += 1;
             }
 
-            ']' => output.push_str(&format!(
+            ']' => output_asm.push_str(&format!(
                 " jmp label{0}start\nlabel{0}end:\n",
                 cola.pop_front().expect("No matching loop")
             )),
@@ -75,14 +75,14 @@ fn main() {
         consecutive = 1;
     }
 
-    output.push_str(EPILOG);
-    fs::write("tmp.asm", &output).expect("Error writing assembly file.");
+    output_asm.push_str(EPILOG);
+    fs::write("tmp.asm", &output_asm).expect("Writing assembly file.");
 
     //assembler
     Command::new("nasm")
         .args(&["-f", "elf64", "tmp.asm", "-o", "tmp.o", "-gdwarf"])
         .status()
-        .expect("Error while generating ELF file.");
+        .expect("Generating ELF file.");
 
     //linker
     Command::new("ld")
@@ -95,15 +95,14 @@ fn main() {
             "/lib64/ld-linux-x86-64.so.2",
         ])
         .status()
-        .expect("Error while linking.");
+        .expect("Linking.");
 
     //execute
     Command::new(&format!("./{}", &args[1].trim_end_matches(".bf")))
         .spawn()
-        .expect("While trying to run generated program.");
+        .expect("Running generated program.");
 }
 
-const EPILOG: &str = " add rsp, 8320\n mov eax, 0\n call exit";
 const PROLOG: &str = r" global _start
  extern getchar
  extern putchar
@@ -118,3 +117,5 @@ _start:
  mov r12, rsp
  sub rsp, 128
 ";
+
+const EPILOG: &str = " add rsp, 8320\n mov eax, 0\n call exit";
